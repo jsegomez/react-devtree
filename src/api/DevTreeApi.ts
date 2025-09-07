4f62e9c652abcc8a215fb8709c0185e081b469a1
jsegomez <jsegomez06@gmail.com>
2025-09-07 16:54:52 -0600

    added interceptor

--- src/api/DevTreeApi.ts
+++ src/api/DevTreeApi.ts
@@ -1,22 +1,12 @@
-import { isAxiosError } from "axios";
 import api from "../utils/axios";
 import { toast } from "sonner";
 import { errorToast } from "../layouts/sonner-alert";
 
 export const getUser = async() => {
     try {
-        const { data } = await api.get("/auth/get-user", {
-            headers: {
-                Authorization: `Bearer ${sessionStorage.getItem('token')}`
-            }
-        });
+        const { data } = await api.get("/auth/get-user");
         return data;
     } catch (error) {
-        if(isAxiosError(error)){
-            const statusCode = error.status;
-            if(statusCode === 401 || statusCode === 404)toast.error("Usuario o contraseña incorrectos", errorToast);            
-        } else {
-            toast.error("Error al crear el usuario", errorToast);
-        }
+        toast.error("Error al obtener el usuario", errorToast);
     }
 }
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,4 +5,23 @@ const api = axios.create({
   baseURL: envConfig.apiUrl,
 });
 
+api.interceptors.request.use(
+    (config) => {
+        const token = sessionStorage.getItem('token');
+        if (token) config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    }
+);
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response.status === 401 || error.response.status === 404) {
+            sessionStorage.removeItem('token');
+            window.location.href = '/auth/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default api;
